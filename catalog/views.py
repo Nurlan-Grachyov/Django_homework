68c1d2caa71ce4d39a0162987d2b775e696796ab
Nurlan <nurlan.grachyov@mail.ru>
2024-12-31 19:31:55 +0400

    ready

---
+++ catalog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'contacts.html')
+
+def home(request):
+    return render(request, "home.html")
