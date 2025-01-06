a088afed460b89be3a191e2bc1486e8ff869f093
Nurlan <nurlan.grachyov@mail.ru>
2025-01-06 15:00:09 +0400

    create migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models import CharField, TextField, ImageField, IntegerField, DateField
 
-# Create your models here.
+
+class Product(models.Model):
+    name = CharField(max_length=100, verbose_name="наименование")
+    description = TextField(verbose_name="описание")
+    image = ImageField(verbose_name="изображение")
+    category = CharField(max_length=100, verbose_name="категория")
+    price = IntegerField(verbose_name="цена за покупку")
+    created_at = DateField(verbose_name="дата создания")
+    updated_at = DateField(verbose_name="дата последнего изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "price", "created_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = CharField(max_length=100, verbose_name="наименование")
+    description = TextField(verbose_name="описание")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
+
 
 def home(request):
     return render(request, "home.html")
