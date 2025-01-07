3d9fca49f8d21a6b1551ace1b25d93b0138f251a
Nurlan <nurlan.grachyov@mail.ru>
2025-01-07 16:46:18 +0400

    seem, ready

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,13 +5,13 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name','price', 'category')
-    list_filter = ('name', 'price')
-    search_fields = ('name', 'description')
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("name", "price")
+    search_fields = ("name", "description")
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    list_filter = ('name',)
-    search_fields = ('name', 'description')
+    list_display = ("id", "name")
+    list_filter = ("name",)
+    search_fields = ("name", "description")
--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -8,34 +8,32 @@ from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
-    help = 'Add test products to the database'
+    help = "Add test products to the database"
 
     def handle(self, *args, **kwargs):
         Category.objects.all().delete()
         Product.objects.all().delete()
-        category_meat = Category.objects.create(name='meat', description='meat')
-        category_instruments = Category.objects.create(name='instruments', description='instruments for garden')
-        product_meat = Product.objects.create(name='beef', description='delicious beef', category=category_meat,
-                                                price=300, created_at='2025-01-06', updated_at='2025-01-06')
-        product_instruments = Product.objects.create(name='shovel', description='iron shovel', category=category_instruments,
-                                                price=500, created_at='2025-01-06', updated_at='2025-01-06')
+        category_meat = Category.objects.create(name="meat", description="meat")
+        category_instruments = Category.objects.create(
+            name="instruments", description="instruments for garden"
+        )
+        product_meat = Product.objects.create(
+            name="beef",
+            description="delicious beef",
+            category=category_meat,
+            price=300,
+            created_at="2025-01-06",
+            updated_at="2025-01-06",
+        )
+        product_instruments = Product.objects.create(
+            name="shovel",
+            description="iron shovel",
+            category=category_instruments,
+            price=500,
+            created_at="2025-01-06",
+            updated_at="2025-01-06",
+        )
 
         create_fixture()
-        fixture_path = 'new_fixture_utf8.json'
-        call_command('loaddata', fixture_path)
-        # Category.objects.all().delete()
-        # Product.objects.all().delete()
-        #
-        # category_fruit, _ = Category.objects.get_or_create(name='fruit')
-        # category_vegetables, _ = Category.objects.get_or_create(name='vegetables')
-        #
-        # with open('catalog_fixture.json', 'r') as json_file:
-        #     products = json.load(json_file)
-        #
-        # for product_data in products:
-        #     if product_data.get('model') == "catalog.product":
-        #         product, created = Product.objects.get_or_create(**product_data['fields'])
-        #         if created:
-        #             self.stdout.write(f'Successfully added products: {product.name}')
-        #         else:
-        #             self.stdout.write(f'products already exists: {product.name}')
+        fixture_path = "new_fixture_utf8.json"
+        call_command("loaddata", fixture_path)
